3f982102e7dd83419a82db739fa08c9484f8933f
Alberto <alberto_trindade2012@hotmail.com>
2025-03-04 23:56:39 -0300

    criando contratos para entidades do SchedulesPerson

---
+++ src/GeekSevenLabs.AdventEcho.Domain/Schedules/ScheduledPersonValidationContract.cs
@@ -0,0 +1,16 @@
+﻿namespace GeekSevenLabs.AdventEcho.Domain.Schedules;
+
+public class ScheduledPersonValidationContract : Contract<ScheduledPerson>
+{
+    public const string ScheduleDayIdRequiredMessage = $"The '{nameof(ScheduledPerson.ScheduleDayId)}' id field is required";
+    public const string AssignedIdRequiredMessage = $"The '{nameof(ScheduledPerson.AssignedId)} id field is required'";
+    public const string AssignmentIdRequiredMessage = $"The '{nameof(ScheduledPerson.AssignedId)} id field is required'";
+    
+    public ScheduledPersonValidationContract(ScheduledPerson scheduledPerson)
+    {
+        Requires()
+            .IsNotEmpty(scheduledPerson.ScheduleDayId.Value, nameof(scheduledPerson.ScheduleDayId), ScheduleDayIdRequiredMessage)
+            .IsNotEmpty(scheduledPerson.AssignedId.Value, nameof(scheduledPerson.AssignedId), AssignedIdRequiredMessage)
+            .IsNotEmpty(scheduledPerson.AssignmentId.Value, nameof(scheduledPerson.AssignmentId), AssignmentIdRequiredMessage);
+    }
+}
