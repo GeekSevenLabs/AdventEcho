aa73993520e599d0d352896ff5eaf71436c9ba2f
Alberto <alberto_trindade2012@hotmail.com>
2025-03-05 21:15:01 -0300

    criando contratos para entidades do ScheduleDayAssignment.cs

---
+++ src/GeekSevenLabs.AdventEcho.Domain/Schedules/ScheduleDayAssignmentValidationContract.cs
@@ -0,0 +1,17 @@
+﻿namespace GeekSevenLabs.AdventEcho.Domain.Schedules;
+
+public class ScheduleDayAssignmentValidationContract : Contract<ScheduleDayAssignment>
+{
+    public const string NameRequiredMessage = 
+        $"The '{nameof(ScheduleDayAssignment.Name)}' field is required";
+
+    public const string ForScheduleTypeRequiredMenssage =
+        $"The '{nameof(ScheduleDayAssignment.ForScheduleType)} field is required'";
+    
+    public ScheduleDayAssignmentValidationContract(ScheduleDayAssignment scheduleDayAssignment)
+    {
+        Requires()
+            .IsNotNullOrEmpty(scheduleDayAssignment.Name, nameof(ScheduleDayAssignment.Name), NameRequiredMessage)
+            .IsNotNull(scheduleDayAssignment.ForScheduleType , nameof(ScheduleDayAssignment.ForScheduleType),ForScheduleTypeRequiredMenssage);
+    }
+}
